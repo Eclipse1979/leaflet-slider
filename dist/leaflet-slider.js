c3d2a4995290423a69cdc8f96258ef2c389f4460
eclipse <eric2.pichon@gmail.com>
2015-02-27 11:03:18 +0100

    added collapse

--- dist/leaflet-slider.js
+++ dist/leaflet-slider.js
@@ -4,21 +4,43 @@ L.Control.Slider = L.Control.extend({
     },
 
     options: {
-        width: '100px',
+        size: '100px',
         position: 'topright',
         min: 0,
         max: 250,
         step: 1,
         id: "slider",
-        value: 50
+        value: 50,
+        collapsed: true,
+        title: 'Slider',
+        logo: 'S'
     },
-    initialize: function (f, options) {
+    initialize: function (f, axis, options) {
         L.setOptions(this, options);
         this.update = f;
+        this.axis = axis;
     },
     onAdd: function (map) {
-        this.sliderbox = L.DomUtil.create('div', 'leaflet-slider-box');
+        this._initLayout();
+        this.update(this.options.value+"");
+        return this.sliderbox;
+    },
+    _updateValue: function () {
+    	this.value = this.slider.value;
+        this.update(this.value);
+    },
+    _initLayout: function () {
+        var className = 'leaflet-control-slider';
+        this.sliderbox = L.DomUtil.create('div', 'leaflet-bar ' +className + ' ' +className + '-' + this.axis);
+        this._sliderLink = L.DomUtil.create('a', className + '-toggle', this.sliderbox);
+        this._sliderLink.setAttribute("title", this.options.title);
+        this._sliderLink.innerHTML = this.options.logo;
+        
+
+
         this.slider = L.DomUtil.create('input', 'leaflet-slider', this.sliderbox);
+        if (this.axis == 'vertical') {this.slider.setAttribute("orient", "vertical");}
+        this.slider.setAttribute("title", this.options.title);
         this.slider.setAttribute("id", this.options.id);
         this.slider.setAttribute("type", "range");
         this.slider.setAttribute("min", this.options.min);
@@ -26,18 +48,40 @@ L.Control.Slider = L.Control.extend({
         this.slider.setAttribute("step", this.options.step);
         this.slider.setAttribute("value", this.options.value);
         this.slider.setAttribute("onChange", this.options.id + "._updateValue()");
-        this.sliderbox.style.width = this.options.width;
+        
         L.DomEvent.disableClickPropagation(this.sliderbox);
-        this.update(this.options.value);
-        return this.sliderbox;
+        if (this.options.collapsed) {
+            if (!L.Browser.android) {
+                L.DomEvent.on(this.sliderbox, {
+                    mouseenter: this._expand,
+                    mouseleave: this._collapse
+                }, this);
+            }
+
+            if (L.Browser.touch) {
+                L.DomEvent
+                    .on(this._sliderLink, 'click', L.DomEvent.stop)
+                    .on(this,_sliderLink, 'click', this._expand, this);
+            } else {
+                L.DomEvent.on(this._sliderLink, 'focus', this._expand, this);
+            }
+        } else {
+            this._expand();
+        }
     },
-    _updateValue: function () {
-    	this.value = this.slider.value;
-        this.update(this.value);
+    _expand: function () {
+        L.DomUtil.addClass(this.sliderbox, 'leaflet-control-slider-expanded');
+        if(this.axis == 'vertical') {this.sliderbox.style.height = this.options.size;}
+        else {this.sliderbox.style.width = this.options.size;}
+    },
+    _collapse: function () {
+        L.DomUtil.removeClass(this.sliderbox, 'leaflet-control-slider-expanded');
+        if(this.axis == 'vertical') {this.sliderbox.style.height = '25px';}
+        else {this.sliderbox.style.width = '26px';}
     }
 
 
 });
-L.control.slider = function (f, options) {
-    return new L.Control.Slider(f, options);
+L.control.slider = function (f, axis, options) {
+    return new L.Control.Slider(f, axis, options);
  };
