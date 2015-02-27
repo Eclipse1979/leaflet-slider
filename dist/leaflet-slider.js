5f31eeff52f7842f499d26d2693e9dc8362a7f8c
eclipse <eric2.pichon@gmail.com>
2015-02-27 15:23:34 +0100

    adapted css to leaflet and added some features
    new options : getValue to change value shown

--- dist/leaflet-slider.js
+++ dist/leaflet-slider.js
@@ -14,11 +14,25 @@ L.Control.Slider = L.Control.extend({
         collapsed: true,
         title: 'Leaflet Slider',
         logo: 'S',
-        axis: 'horizontal'
+        axis: 'horizontal',
+        getValue: function(value) {
+            return value;
+        }
     },
     initialize: function (f, options) {
         L.setOptions(this, options);
-        this.update = f;
+        if (typeof f == "function") {
+            this.update = f;
+        } else {
+            this.update = function (value) {
+                console.log(value);
+            };
+        }
+        if (typeof this.options.getValue != "function") {
+            this.options.getValue = function (value) {
+                return value;
+            };
+        }
         if (this.options.axis!='vertical') {
             this.options.axis = 'horizontal';
         }
@@ -29,18 +43,17 @@ L.Control.Slider = L.Control.extend({
         return this._container;
     },
     _updateValue: function () {
-    	this._sliderValue.innerHTML = this.value = this.slider.value;
+    	this._sliderValue.innerHTML = this.options.getValue(this.value = this.slider.value);
         this.update(this.value);
     },
     _initLayout: function () {
         var className = 'leaflet-control-slider';
-        this._container = L.DomUtil.create('div', 'leaflet-bar ' +className + ' ' +className + '-' + this.options.axis);
+        this._container = L.DomUtil.create('div', className + ' ' +className + '-' + this.options.axis);
         this._sliderLink = L.DomUtil.create('a', className + '-toggle', this._container);
         this._sliderLink.setAttribute("title", this.options.title);
         this._sliderLink.innerHTML = this.options.logo;
         
         this._sliderValue = L.DomUtil.create('p', className+'-value', this._container);
-        this._sliderValue.innerHTML = this.options.value;
 
         this.slider = L.DomUtil.create('input', 'leaflet-slider', this._container);
         if (this.options.axis == 'vertical') {this.slider.setAttribute("orient", "vertical");}
@@ -52,10 +65,11 @@ L.Control.Slider = L.Control.extend({
         this.slider.setAttribute("step", this.options.step);
         this.slider.setAttribute("value", this.options.value);
         this.slider.setAttribute("onChange", this.options.id + "._updateValue()");
-        if (this.options.axis =='vertical') {this.slider.style.height = (this.options.size.replace('px', '') -26) +'px';}
-        else {this.slider.style.width = (this.options.size.replace('px', '') -45) +'px';}
+        if (this.options.axis =='vertical') {this.slider.style.height = (this.options.size.replace('px','') -26) +'px';}
+        else {this.slider.style.width = (this.options.size.replace('px','') -45) +'px';}
         
         L.DomEvent.disableClickPropagation(this._container);
+        this._sliderValue.innerHTML = this.options.getValue(this.slider.value);
         if (this.options.collapsed) {
             if (!L.Browser.android) {
                 L.DomEvent.on(this._container, {
@@ -77,13 +91,9 @@ L.Control.Slider = L.Control.extend({
     },
     _expand: function () {
         L.DomUtil.addClass(this._container, 'leaflet-control-slider-expanded');
-        if(this.options.axis == 'vertical') {this._container.style.height = this.options.size;}
-        else {this._container.style.width = this.options.size;}
     },
     _collapse: function () {
         L.DomUtil.removeClass(this._container, 'leaflet-control-slider-expanded');
-        if(this.options.axis == 'vertical') {this._container.style.height = '25px';}
-        else {this._container.style.width = '26px';}
     }
 
 
