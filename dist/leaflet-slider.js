916a01ddc7b4179b0a410e7e532b17d442727b09
eclipse <eric2.pichon@gmail.com>
2015-02-27 12:18:08 +0100

    added a dynamic value indicator

--- dist/leaflet-slider.js
+++ dist/leaflet-slider.js
@@ -18,27 +18,33 @@ L.Control.Slider = L.Control.extend({
     initialize: function (f, axis, options) {
         L.setOptions(this, options);
         this.update = f;
-        this.axis = axis;
+        if (axis == 'horizontal' || axis =='vertical') {
+            this.axis = axis;
+        } else {
+            console.log("Orientation of the slider is not correct : it should be 'horizontal' or 'vertical'. 'horizontal is taken as default'");
+            this.axis = 'horizontal';
+        }
     },
     onAdd: function (map) {
         this._initLayout();
         this.update(this.options.value+"");
-        return this.sliderbox;
+        return this._container;
     },
     _updateValue: function () {
-    	this.value = this.slider.value;
+    	this._sliderValue.innerHTML = this.value = this.slider.value;
         this.update(this.value);
     },
     _initLayout: function () {
         var className = 'leaflet-control-slider';
-        this.sliderbox = L.DomUtil.create('div', 'leaflet-bar ' +className + ' ' +className + '-' + this.axis);
-        this._sliderLink = L.DomUtil.create('a', className + '-toggle', this.sliderbox);
+        this._container = L.DomUtil.create('div', 'leaflet-bar ' +className + ' ' +className + '-' + this.axis);
+        this._sliderLink = L.DomUtil.create('a', className + '-toggle', this._container);
         this._sliderLink.setAttribute("title", this.options.title);
         this._sliderLink.innerHTML = this.options.logo;
         
+        this._sliderValue = L.DomUtil.create('p', className+'-value', this._container);
+        this._sliderValue.innerHTML = this.options.value;
 
-
-        this.slider = L.DomUtil.create('input', 'leaflet-slider', this.sliderbox);
+        this.slider = L.DomUtil.create('input', 'leaflet-slider', this._container);
         if (this.axis == 'vertical') {this.slider.setAttribute("orient", "vertical");}
         this.slider.setAttribute("title", this.options.title);
         this.slider.setAttribute("id", this.options.id);
@@ -48,11 +54,13 @@ L.Control.Slider = L.Control.extend({
         this.slider.setAttribute("step", this.options.step);
         this.slider.setAttribute("value", this.options.value);
         this.slider.setAttribute("onChange", this.options.id + "._updateValue()");
+        if (this.axis =='vertical') {this.slider.style.height = (this.options.size.replace('px', '') -26) +'px';}
+        else {this.slider.style.width = (this.options.size.replace('px', '') -45) +'px';}
         
-        L.DomEvent.disableClickPropagation(this.sliderbox);
+        L.DomEvent.disableClickPropagation(this._container);
         if (this.options.collapsed) {
             if (!L.Browser.android) {
-                L.DomEvent.on(this.sliderbox, {
+                L.DomEvent.on(this._container, {
                     mouseenter: this._expand,
                     mouseleave: this._collapse
                 }, this);
@@ -70,14 +78,14 @@ L.Control.Slider = L.Control.extend({
         }
     },
     _expand: function () {
-        L.DomUtil.addClass(this.sliderbox, 'leaflet-control-slider-expanded');
-        if(this.axis == 'vertical') {this.sliderbox.style.height = this.options.size;}
-        else {this.sliderbox.style.width = this.options.size;}
+        L.DomUtil.addClass(this._container, 'leaflet-control-slider-expanded');
+        if(this.axis == 'vertical') {this._container.style.height = this.options.size;}
+        else {this._container.style.width = this.options.size;}
     },
     _collapse: function () {
-        L.DomUtil.removeClass(this.sliderbox, 'leaflet-control-slider-expanded');
-        if(this.axis == 'vertical') {this.sliderbox.style.height = '25px';}
-        else {this.sliderbox.style.width = '26px';}
+        L.DomUtil.removeClass(this._container, 'leaflet-control-slider-expanded');
+        if(this.axis == 'vertical') {this._container.style.height = '25px';}
+        else {this._container.style.width = '26px';}
     }
 
 
